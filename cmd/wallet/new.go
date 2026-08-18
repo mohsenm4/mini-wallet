a32bcb024a9adc03c129dfa0694fbdf776342480
mohsen <mohsen.salehi2070@gmail.com>
2026-08-18 16:25:36 +0330

    add newMnemonic to RunE cmd

--- cmd/wallet/new.go
+++ cmd/wallet/new.go
@@ -3,18 +3,27 @@ package main
 import (
 	"fmt"
 
+	"github.com/mohsenm4/mini-wallet/internal/mnemonic"
 	"github.com/spf13/cobra"
 )
 
+var newBits int
+
 func init() {
 	rootCmd.AddCommand(newCmd)
+	newCmd.Flags().IntVar(&newBits, "bits", 128, "entropy size in bits")
 }
 
 var newCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Generate a new mnemonic",
 	Long:  `Generate a new mnemonic phrase for an Ethereum HD wallet.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("TODO: generate mnemonic")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		m, err := mnemonic.NewMnemonic(newBits)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Generated mnemonic:", m)
+		return nil
 	},
 }
